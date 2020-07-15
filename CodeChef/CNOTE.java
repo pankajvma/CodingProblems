170901750486c15cd8d91f6e6e6bf4dd2c782131
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-07-15 13:11:42 +0530

    Solution to the problem "CNOTE" added
    Solved using object array in java

---
+++ CodeChef/CNOTE.java
@@ -0,0 +1,51 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CNote
+{
+	public static void main (String[] args) throws IOException
+	{
+	    try {
+	        Scanner sc=new Scanner(System.in);
+    		int noOfTestCases=0,reqPage=0,availPage=0,budget,noOfNotebooks;
+    		noOfTestCases=sc.nextInt();
+    		boolean lucky=false;
+    		for(int i=1;i<=noOfTestCases;i++)
+    		{
+    			lucky=false;
+    			reqPage=sc.nextInt();
+    			availPage=sc.nextInt();
+    			reqPage=reqPage-availPage;
+    			budget=sc.nextInt();
+    			noOfNotebooks=sc.nextInt();
+    			Notebook[] nb=new Notebook[noOfNotebooks];
+    			for(int j=0;j<noOfNotebooks;j++)
+    			{
+    				nb[j]=new Notebook();
+    				nb[j].pages=sc.nextInt();
+    				nb[j].cost=sc.nextInt();
+    			}
+    			for(int j=0;j<noOfNotebooks;j++)
+    			{
+    				if(nb[j].pages>=reqPage&&nb[j].cost<=budget)
+    					lucky=true;
+    			}
+    			if(lucky)
+    				System.out.println("LuckyChef");
+    			else
+    				System.out.println("UnluckyChef");
+    		}
+	    } catch(Exception e) {
+	        return;
+	    } 
+		
+	}
+}
+class Notebook
+{
+	int pages,cost;
+}
