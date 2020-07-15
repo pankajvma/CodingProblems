3e7983b0cc9fa05a2c976d06fb956691fc1ddf44
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-07-15 13:09:31 +0530

    Solution to the problem "LECANDY" added
    cakewalk

---
+++ CodeChef/LECANDY.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class CodeChef
+{
+	public static void main (String[] args)
+	{
+	    try 
+	    {
+	        Scanner sc=new Scanner(System.in);
+		    int testCase=sc.nextInt(),noOfEleph,noOfCandies,total;
+    		for(int i=1;i<=testCase;i++)
+    		{
+        		noOfEleph=sc.nextInt();
+        		noOfCandies=sc.nextInt();
+    			int[] A=new int[noOfEleph];
+    			for(int j=0;j<noOfEleph;j++)
+    				A[j]=sc.nextInt();
+    			total=0;
+    			for(int j=0;j<noOfEleph;j++)
+    				total+=A[j];
+    			if(total>noOfCandies)
+    				System.out.println("No");
+    			else
+    				System.out.println("Yes");
+    		}
+	    } 
+	    catch(Exception e) 
+	    {
+	        return;
+	    }
+		
+	}
+}
