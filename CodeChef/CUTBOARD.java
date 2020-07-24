110ee170a856a7656a22f1299a6eb54386e35ba3
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-07-24 14:59:00 +0530

    Update CUTBOARD.java
    Reduced LOC

--- CodeChef/CUTBOARD.java
+++ CodeChef/CUTBOARD.java
@@ -1,27 +1,19 @@
 import java.util.*;
 import java.lang.*;
-import java.io.*;
 
-class CUTBOARD
+class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String args[])throws java.lang.Exception
 	{
 	    try{
-    		short no_of_cuts=0, test_cases=0,n=0,m=0, tCount=0;
-    		Scanner sc=new Scanner(System.in);
-    		test_cases= sc.nextShort();
-    		for(int i=0;i<test_cases;i++){
-    		    n= sc.nextShort();
-    		    m= sc.nextShort();
-    		    System.out.print((n-1)*(m-1));
-    		    if(tCount==(test_cases-1))
-					    break;
-				    System.out.print("\n");
-		    }
+        	Scanner sc=new Scanner(System.in);
+        	short test_cases= sc.nextShort();
+        	for(int i=0;i<test_cases;i++){
+        	    short n= sc.nextShort();
+        	    short m= sc.nextShort();
+        	    System.out.println((--n)*(--m));
+    		}
 	    }
-		    catch(Exception e)
-		    {
-		        
-		    }
+	    catch(Exception e){}
 	}
 }
