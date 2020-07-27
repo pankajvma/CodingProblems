168e91dba4b299f3300a160e0bd3b812a72af979
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-07-27 13:31:28 +0530

    Check if VAalid Mountain Array
    Given an array A of integers, return true if and only if it is a valid mountain array.

---
+++ LeetCode/ValidMountainArray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validMountainArray(int[] A) {
+        int len= A.length-1, j=0;
+        if(len==0||len<0||A[1]<A[0])
+            return false;
+        for(int i=0; i<len; i++){
+            if(A[i]<A[i+1]&&(i+1)==len)
+                return false;
+            else if(A[i] < A[i+1])
+                continue;
+            else if(A[i] > A[i+1]){
+                j = i+1;
+                break;
+            }
+            else
+                return false;    
+        }
+        while(j<len){
+            if(A[j] > A[j+1]){
+              j++;  
+              continue;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+}
