329626b433b8243c0c02c181b9f74dac8e28697a
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-07-28 11:24:49 +0530

    MoveZeroes Array problem
    Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
        You must do this in-place without making a copy of the array.
        Minimize the total number of operations.

---
+++ LeetCode/MoveZeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len= nums.length-1, zeroCounts=0;
+        for(int i = 0; i<=len; i++)
+            if(nums[i]==0)
+                zeroCounts++;
+            else
+                nums[i-zeroCounts] = nums[i];
+        while(zeroCounts!=0)
+            nums[len-(--zeroCounts)] = 0;
+    }
+}
