4fb2bbc0f9dfa01f58b29ed1eea3d166d0f46263
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-08-01 22:44:32 +0530

    Update SNCKYEAR.java

--- CodeChef/SNCKYEAR.java
+++ CodeChef/SNCKYEAR.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class SNCKYEAR
 {
 	public static void main (String[] args) throws java.lang.Exception
