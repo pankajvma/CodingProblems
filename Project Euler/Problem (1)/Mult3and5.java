a12d310bf73a777973cfe4843dcd6aa97e644f70
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-08-09 19:32:08 +0530

    Logic improved

--- Project Euler/Problem (1)/Mult3and5.java
+++ Project Euler/Problem (1)/Mult3and5.java
@@ -9,14 +9,9 @@ public class Mult3and5
     public static void main(String[] args) {
 		int result=0;
 		long startTime=System.nanoTime();		//To check execution time
-		for(int i=0;i<1000;i++){ 
-			if(i%3==0&&i%5==0)
-				result+=i;	
-			else if(i%3==0&&i%5!=0)  
+		for(int i=0;i<1000;i++)
+			if(i%3==0||i%5==0)
 				result+=i;
-			else if(i%3!=0&&i%5==0)  
-				result+=i;
-		}
 		System.out.println(result );
 		long endTime=System.nanoTime();		//To check execution time
 		long totalTime=endTime-startTime;
