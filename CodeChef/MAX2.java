d51d08dd122b59320fc4f8c9b8dc8c28f985f1a3
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-08-23 21:06:18 +0530

    Update MAX2.java

--- CodeChef/MAX2.java
+++ CodeChef/MAX2.java
@@ -16,3 +16,25 @@ class MAX2{
 
 	}
 }
+
+/* Alternate Solution
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.Math;
+class MAX2{
+	public static void main(String args[])throws Exception{
+		try{
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+			int test = Integer.parseInt(bfr.readLine());
+			String binaryString = bfr.readLine();
+			int number = Integer.parseInt(binaryString, 2);
+			int pwr = 0;
+			while(number % Math.pow(2, pwr) == 0)
+				pwr++;
+			System.out.println(number);
+			System.out.println(pwr - 1);
+		}catch(Exception e){}
+
+	}
+}*/
