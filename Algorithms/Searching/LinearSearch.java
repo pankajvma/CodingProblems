08e1e4bba81835139831a5cbf5a9d071c6226be9
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2020-08-27 22:39:26 +0530

    Create LinearSearch.java

---
+++ Algorithms/Searching/LinearSearch.java
@@ -0,0 +1,32 @@
+package vma.Searching;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Enter the number of elements:");
+        int numberOfElements = scanner.nextInt();
+        System.out.println("Enter the "+ numberOfElements +" elements:");
+        int [] array = getElements(numberOfElements);
+        System.out.println("Enter the number to search in the array:");
+        int numberToSearch = scanner.nextInt();
+        linearSearch(array, numberToSearch);
+    }
+
+    private static int[] getElements(int numberOfElements){
+        int [] array = new int [numberOfElements];
+        for(int i = 0; i < numberOfElements; i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    private static void linearSearch(int [] array, int numberToSearch){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == numberToSearch){
+                System.out.println(numberToSearch+ " found at index "+ i);
+            }
+        }
+    }
+}
