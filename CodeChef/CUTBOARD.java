2817633721789063bd1259335d8b70a3213f2580
pankajvma <pankajvma@gmail.com>
2020-11-20 10:05:04 +0530

    Renamed CUTBOARD, LECANDY classnames

--- CodeChef/CUTBOARD.java
+++ CodeChef/CUTBOARD.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 
-class Codechef
+class CUTBOARD
 {
 	public static void main (String args[])throws java.lang.Exception
 	{
--- CodeChef/LECANDY.java
+++ CodeChef/LECANDY.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class CodeChef
+class LECANDY
 {
 	public static void main (String[] args)
 	{
