cd76a09e96a21104ca95f0ceecea47894aa8d6b7
pankajvma <pankajvma@gmail.com>
2020-11-20 21:55:02 +0530

    Recursive method added

--- DSA Practice/ReverseArray.java
+++ DSA Practice/ReverseArray.java
@@ -1,18 +1,31 @@
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 4, 5};
         int end = arr.length - 1;
         int start = 0;
         int temp;
-        while(start < end){
+        reverseArray(arr, start, end);
+
+        //Iterative method
+        /*while(start < end){
             temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
             end--;
-        }
+        }*/
         for (int j : arr){
             System.out.println(j);
+        }
+    }
+
+    //Recursive Method
+    public static void reverseArray(int[] arr, int start, int end){
+        if(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            reverseArray(arr, ++start, --end);
         }
     }
 }
