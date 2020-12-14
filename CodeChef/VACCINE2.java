b061abaeffe67971c498114eae93062122bae42b
pankajvma <pankajvma@gmail.com>
2020-12-14 12:26:08 +0530

    December long challenge

---
+++ CodeChef/VACCINE2.java
@@ -0,0 +1,152 @@
+import java.io.*;
+
+class VACCINE2 {
+    static class FastReader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader fs = new FastReader();
+            int testCases = fs.nextInt();
+            while (testCases != 0) {
+                int N = fs.nextInt();
+                int D = fs.nextInt();
+                int peopleInRisk = 0;
+                int outOfRisk = 0;
+                int noOfDays = 0;
+                int a;
+                for(int i = N; i > 0; i--){
+                    a = fs.nextInt();
+                    if(a <= 9 || a >= 80){
+                        peopleInRisk++;
+                    }
+                }
+                outOfRisk = N - peopleInRisk;
+
+
+                if (outOfRisk % D == 0) {
+                    noOfDays = outOfRisk / D;
+                } else {
+                    noOfDays = outOfRisk / D + 1;
+                }
+
+                if (peopleInRisk % D == 0) {
+                    noOfDays += peopleInRisk / D;
+                } else {
+                    noOfDays += peopleInRisk / D + 1;
+                }
+                System.out.println(noOfDays);
+
+                testCases--;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
