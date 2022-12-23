df905d83fc2bbc5ac5c9573519e7dd486b3f0a40
pankajvma <pankajvma@gmail.com>
2022-12-23 09:36:19 +0530

    Java Scanner input using next*(), nextLine()

--- HackerRank/WarmUpChallenges/java/easy/JavaIfElse.java
+++ HackerRank/WarmUpChallenges/java/easy/JavaIfElse.java
@@ -1,13 +1,7 @@
 // https://www.hackerrank.com/challenges/java-if-else/problem
 package hackerrank.warmupChallenges.java.easy;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class JavaIfElse {
     private static final Scanner scanner = new Scanner(System.in);
