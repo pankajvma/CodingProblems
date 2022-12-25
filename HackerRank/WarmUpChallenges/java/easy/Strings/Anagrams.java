21ada3eb265562373624c3532bf44f5f2a0f87c2
pankajvma <pankajvma@gmail.com>
2022-12-25 11:53:46 +0530

    Detect anagram

---
+++ HackerRank/WarmUpChallenges/java/easy/Strings/Anagrams.java
@@ -0,0 +1,45 @@
+// https://www.hackerrank.com/challenges/java-anagrams/problem
+package hackerrank.warmupChallenges.java.easy.Strings;
+
+import java.util.Scanner;
+
+public class Anagrams {
+    
+    static int[] aToZ(){
+        int[] arr = new int[26];
+        int i = 0;
+        for(int alphabet = 'a'; alphabet <= 'z'; alphabet++){
+            arr[i] = alphabet;
+            i++;
+        }
+        return arr;
+    }
+
+    static int getCharCount(int alphabet, String aOrb){
+        int count = 0;
+        for(int i = 0; i < aOrb.length(); i++){
+            if(aOrb.charAt(i) == alphabet) count++;
+        }
+        return count;
+    }
+    
+    static boolean isAnagram(String a, String b) {
+        int[] charArr = aToZ();
+        for(int i = 0; i < charArr.length; i++){
+            if(getCharCount(charArr[i], a.toLowerCase()) != getCharCount(charArr[i], b.toLowerCase())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
---
+++ HackerRank/WarmUpChallenges/java/easy/Strings/Palindrome.java
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/java-string-reverse/problem
+package hackerrank.warmupChallenges.java.easy.Strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int low = 0;
+        int up = A.length()-1;
+        boolean isPalin = true;
+        while(low < up){
+            if(A.charAt(low) != A.charAt(up)){
+                isPalin = false;
+                break;
+            }
+            low++;up--;
+        }
+        if(isPalin) System.out.println("Yes");
+        else System.out.println("No");
+        /* Enter your code here. Print output to STDOUT. */
+        
+    }
+}
