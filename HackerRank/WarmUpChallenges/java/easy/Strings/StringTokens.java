446bf0555ab8cb0bb9dabc76d7753e2b82003fa0
pankajvma <pankajvma@gmail.com>
2022-12-25 19:03:24 +0530

    String tokens

---
+++ HackerRank/WarmUpChallenges/java/easy/Strings/StringTokens.java
@@ -0,0 +1,22 @@
+
+package hackerrank.warmupChallenges.java.easy.Strings;
+
+import java.util.Scanner;
+
+public class StringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        if(s.trim().length() == 0){  //To check empty string
+            System.out.println(0);
+        } else{
+            String[] sArr = s.trim().split("[ !,?._'@]+"); //trimming the string to remove trailing spaces
+            System.out.println(sArr.length);
+            for(Object obj : sArr)
+                System.out.println(obj);
+        }
+        // Write your code here.
+        scan.close();
+    }
+}
