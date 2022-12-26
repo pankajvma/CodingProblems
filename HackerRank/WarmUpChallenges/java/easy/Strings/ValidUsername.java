1c319d3934298a486e66a035b65a939e80d6cec9
pankajvma <pankajvma@gmail.com>
2022-12-26 14:38:52 +0530

    Day 3 Pattern matching with regex with explanation

---
+++ HackerRank/WarmUpChallenges/java/easy/Strings/ValidUsername.java
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/valid-username-checker/problem
+package hackerrank.warmupChallenges.java.easy.Strings;
+
+import java.util.Scanner;
+
+public class ValidUsername {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
+
+class UsernameValidator {
+
+    public static final String regularExpression = "[a-zA-Z]([a-zA-Z]|[0-9]|_){7,29}";
+
+    /* Explaining above regularExpression
+    #1. [a-zA-Z] ----------------> Should start with Any upper or lower case letter
+    #2. ([a-zA-Z]|[0-9]|_) ------> Followed by any upper case or lower case letter or any digit or _
+    #3. {7,29} ------------------> Prethe preceeding token i. e. #2 should have a length between 7-29
+    #4. Since we are already taking step1 into consideration we have specified the length as 7-29 in #3
+     */
+}
--- HackerRank/WarmUpChallenges/java/medium/Strings/Regex.java
+++ HackerRank/WarmUpChallenges/java/medium/Strings/Regex.java
@@ -18,5 +18,19 @@ public class Regex {
 class MyRegex{
     String ipDigitPattern = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
     String pattern = ipDigitPattern + "[.]" + ipDigitPattern + "[.]" + ipDigitPattern + "[.]" + ipDigitPattern ;
+
+    /* Explaining above ipDigitPattern
+    #1. \d{1,2} ----------------> Any 2 digit number
+    #2. | ----------------------> OR
+    #3. (0|1)\\d{2} ------------> 0 or 1 followed by any 2 digit number
+    #4. | ----------------------> OR
+    #5. 2[0-4]\\d ------------------> 2 followed by a number between 0 to 4(inclusive), folowed by any one digit number (To handle numbers between 200 to 249)
+    #6. | ----------------------> OR
+    #7. 25[0-5]  ---------------> 25 followed by a number between 0 to 5. (To handle numbers between 250 to 255)
+    #8. Enclose #1 to #7 within () to group them in a single regex statement since we have to use it with [.] to create the full pattern
+
+    Explaining pattern
+    Utilizing ipDigitPattern and using it with [.] to create the full pattern
+     */
     
 }
