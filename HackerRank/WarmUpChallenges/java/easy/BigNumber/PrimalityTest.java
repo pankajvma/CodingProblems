c949e9e134084430ad9433f5b63b06af52dfdc9c
pankajvma <pankajvma@gmail.com>
2022-12-27 10:20:26 +0530

    Adding primality test with math package

---
+++ HackerRank/WarmUpChallenges/java/easy/BigNumber/PrimalityTest.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/java-primality-test/problem
+package hackerrank.warmupChallenges.java.easy.BigNumber;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class PrimalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger bigNumber = new BigInteger(n);
+        if(bigNumber.isProbablePrime(1)){
+            System.out.println("prime");
+        }
+        else{
+            System.out.println("not prime");
+        }
+        bufferedReader.close();
+    }
+}
