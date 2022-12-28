508d18b084d814babf98b29e16e3dd75901530d6
pankajvma <pankajvma@gmail.com>
2022-12-28 18:02:36 +0530

    Day5 D Array

---
+++ HackerRank/WarmUpChallenges/java/easy/DataStructures/OneDArray.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/java-1d-array-introduction/problem
+package hackerrank.warmupChallenges.java.easy.DataStructures;
+
+import java.util.Scanner;
+
+public class OneDArray {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
