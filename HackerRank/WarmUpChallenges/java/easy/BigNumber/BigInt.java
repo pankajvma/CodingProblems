4d3a79fb57648cd38508e0ebba4f4354998ff496
pankajvma <pankajvma@gmail.com>
2022-12-28 11:59:18 +0530

    Day5 BigInt addition and multiplication

---
+++ HackerRank/WarmUpChallenges/java/easy/BigNumber/BigInt.java
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/java-biginteger/problem
+package hackerrank.warmupChallenges.java.easy.BigNumber;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigInt {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        BigInteger bigNum1 = new BigInteger(scn.nextLine());
+        BigInteger bigNum2 = new BigInteger(scn.nextLine());
+        System.out.println(bigNum1.add(bigNum2));
+        System.out.println(bigNum1.multiply(bigNum2));
+        scn.close();
+    }
+}
