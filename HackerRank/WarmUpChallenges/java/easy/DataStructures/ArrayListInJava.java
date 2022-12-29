1eecccd85b8d176b9c28479dffb4073bfbc165fb
pankajvma <pankajvma@gmail.com>
2022-12-29 17:44:56 +0530

    Optimizing code with Exception handling

--- HackerRank/WarmUpChallenges/java/easy/DataStructures/ArrayListInJava.java
+++ HackerRank/WarmUpChallenges/java/easy/DataStructures/ArrayListInJava.java
@@ -22,12 +22,19 @@ public class ArrayListInJava {
         for(int i = 0; i < queries; i++){
             int x = scn.nextInt() - 1;
             int y = scn.nextInt() - 1;
+            try{
+                System.out.println(arr.get(x).get(y));
+            } catch(IndexOutOfBoundsException e){
+                System.out.println("ERROR!");
+            }
+            /* Another way of handling the exception
             if((x + 1 > n) || (y +1 > arr.get(x).size())){
                 System.out.println("ERROR!");
             }
             else{
                 System.out.println(arr.get(x).get(y));
             }
+             */
         }
     }
 }
