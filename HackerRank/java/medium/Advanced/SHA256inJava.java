50b77231445159959b0ab51c9dab3f2430b01525
pankajvma <pankajvma@gmail.com>
2023-01-15 17:22:55 +0530

    Day22 SHA-256

---
+++ HackerRank/java/medium/Advanced/SHA256inJava.java
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/sha-256/problem
+package hackerrank.java.medium.Advanced;
+
+import java.util.*;
+import java.security.MessageDigest;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256inJava {
+
+    public static void main(String[] args) {
+        System.out.println(printMD5(new Scanner(System.in).next()));        
+    }
+    
+    public static String printMD5(String message){
+        try{
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+ 
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(message.getBytes());
+ 
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+ 
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 64) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch(NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
