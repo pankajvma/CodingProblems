d38e8d0b414895586b4f1af6007d53a3c7205e76
pankajvma <pankajvma@gmail.com>
2023-01-19 23:20:19 +0530

    Day3 PS

---
+++ HackerRank/problemSolving/easy/implementation/BeautifulDays.java
@@ -0,0 +1,54 @@
+// https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+package hackerrank.problemSolving.easy.implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+
+    public static int reverse(int i){
+        int reverse = 0;
+        while(i != 0){
+            reverse = (reverse * 10 + i % 10);
+            i /= 10;
+        }
+        return reverse;
+    }
+
+    public static int beautifulDays(int i, int j, int k) {
+        int countBeautifulDays = 0;
+        for(int date = i; date <= j; date++){
+            if(Math.abs(date - reverse(date)) % k ==0){
+                countBeautifulDays++;
+            }
+        }
+        return countBeautifulDays;
+    }
+
+}
+
+public class BeautifulDays {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ HackerRank/problemSolving/medium/implementation/ExtraLongFactorial.java
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem
+package hackerrank.problemSolving.medium.implementation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.math.BigInteger;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void extraLongFactorials(int n) {
+        BigInteger bigFactorial = new BigInteger("1");
+        for(int i = 2; i <= n; i++){
+            bigFactorial = bigFactorial.multiply(new BigInteger(Integer.toString(i)));
+        }
+        System.out.println(bigFactorial);
+    }
+
+}
+
+public class ExtraLongFactorial {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
