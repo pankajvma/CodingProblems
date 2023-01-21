c01d64d19db7b9b653e65a7d8196a08a0149df71
pankajvma <pankajvma@gmail.com>
2023-01-21 18:27:26 +0530

    Day5 PS

--- HackerRank/problemSolving/easy/algorithms/sorting/insertion/InsertionSortPart2.java
+++ HackerRank/problemSolving/easy/algorithms/sorting/insertion/InsertionSortPart2.java
@@ -1,4 +1,4 @@
-// https://www.hackerrank.com/challenges/insertionsort1/problem
+// https://www.hackerrank.com/challenges/insertionsort2/proble/m
 package hackerrank.problemSolving.easy.algorithms.sorting.insertion;
 
 import java.io.*;
@@ -7,32 +7,32 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.toList;
 
 class Result {
-    
+
     public static void printList(int n, List<Integer> arr){
         System.out.print(arr.get(0));
         for(int j = 1; j < n; j++){
             System.out.print(" "+ arr.get(j));
-        }
+        } System.out.println();
     }
 
-    public static void insertionSort1(int n, List<Integer> arr) {
-        int num = arr.get(n-1); // As per the problem, whole list is sorted, last element is misplaced
-        for(int i = n - 1; i >= 0; i--){
-            if(i == 0 || arr.get(i - 1) < num){
-                arr.set(i, num);
-                printList(n, arr);
-                break;
-            } else{
-                arr.set(i, arr.get(i - 1));
+    public static void insertionSort2(int n, List<Integer> arr) { //modifies method insertionSort1 from InsertionSortPart1
+        for(int index = 1; index < n; index++){
+            int num = arr.get(index);
+            for(int i = index; i >= 0; i--){
+                if(i == 0 || arr.get(i - 1) < num){
+                    arr.set(i, num);
+                    printList(n, arr);
+                    break;
+                } else{
+                    arr.set(i, arr.get(i - 1));
+                }              
             }
-            printList(n, arr);
-            System.out.println();
         }
     }
 
 }
 
-public class InsertionSortPart1 {
+public class InsertionSortPart2 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -42,7 +42,7 @@ public class InsertionSortPart1 {
             .map(Integer::parseInt)
             .collect(toList());
 
-        Result.insertionSort1(n, arr);
+        Result.insertionSort2(n, arr);
 
         bufferedReader.close();
     }
