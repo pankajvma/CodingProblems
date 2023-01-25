cf5e20d203c09247db44d9b8f4ce7337c82bc11a
pankajvma <pankajvma@gmail.com>
2023-01-25 22:39:14 +0530

    adding optimised gcd for assingnement2 problem

--- dsAlgoSpecialization/week2efficientAlgorithms/GCD.java
+++ dsAlgoSpecialization/week2efficientAlgorithms/GCD.java
@@ -4,31 +4,20 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GCD {
-    public static void main(String[] args){
-        // Scanner scn = new Scanner(System.in);
-        // int a = scn.nextInt();
-        // int b = scn.nextInt();
-        // scn.close();
-        Random rnd = new Random(5);
-        // for(int i = 0; i < 10; i++){
-            int a = (rnd.nextInt(10)+10)*2;
-            int b = (rnd.nextInt(10)+10)*3;
-            // System.out.println("GCD of "+ a + " and "+ b +"= "+ getGCD(a, b));
-            System.out.println("GCD of "+ a + " and "+ b +"= "+ getGCD(a, b));
-        // }
-        // int a = rnd.nextInt();
-        // int b = rnd.nextInt();
-        // System.out.println("GCD of "+ a + "and "+ b +"= "+ getGCD(a, b));
-    }
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+    
+        System.out.println(getGCD(a, b));
+      }
+    
 
-    public static int getGCD(int a, int b){
-        int gcd = 0;
-        int limit = a < b ? a : b;
-        for(int i = 1; i <= limit; i++){
-            if(a % i == 0 && b % i == 0){
-                gcd = i;
-            }
+    public static int getGCD(int a, int b){ // Fast algorithm using Euclidean lemma
+        System.out.println(a+ " "+b);
+        if(b == 0){
+            return a;
         }
-        return gcd;
+        return getGCD(b, a%b);
     }
 }
