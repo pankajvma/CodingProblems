275cdd1939c46b373a53a3c8e6fe08895d847ac5
pankajvma <pankajvma@gmail.com>
2023-01-31 23:16:50 +0530

    final solution for CarFueling problem

--- dsAlgoSpecialization/week3greedyAlgorithms/CarFueling.java
+++ dsAlgoSpecialization/week3greedyAlgorithms/CarFueling.java
@@ -4,28 +4,23 @@ import java.util.Scanner;
 
 public class CarFueling {
     static int computeMinRefills(int dist, int tank, int[] stops) {
-        // The minimum number of refills to get from one city to another if a car can travel at most "tank" miles on a full tank.
-        // The distance between the cities is "dist" miles and there are gas stations at distances stop1, stop2,..., stopn along the way. 
-        // We assume that a car starts with a full tank.
         int currentStop = 0;
-        int lastStop = 0;
         int count = 0;
         int totalStops = stops.length;
-        for(int i = 0; i < totalStops; i++){
-            if(dist <= tank){
+
+        for (int i = 0; i < totalStops; i++) {
+            if (dist - currentStop <= tank) {
                 return count;
-            } else if((stops[i] - currentStop) > tank && (i - 1) >= 0 && (stops[i - 1] - currentStop) <= tank){
-                lastStop = currentStop;
-                currentStop = stops[i - 1];
-                dist -= (currentStop - lastStop);
-                count++;
-            } else if(i == totalStops - 1){
+            } else if (((i + 1) <= (totalStops - 1) && (stops[i + 1] - currentStop) > tank
+                    && (stops[i] - currentStop) <= tank)
+                    ||
+                    ((i == totalStops - 1) && (stops[i] - currentStop) <= tank)) {
                 currentStop = stops[i];
-                dist -= currentStop;
                 count++;
             }
         }
-        if(dist > tank) return -1;
+
+        if(dist - currentStop > tank) return -1;
         return count;
     }
 
@@ -34,15 +29,10 @@ public class CarFueling {
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
-        int stops[] = new int[n];
-        // int dist = 950;
-        // int tank = 400;
-        // int n = 4;
-        // int[] stops = {200, 375, 550, 750};
-        for (int i = 0; i < n; i++) {
+        int[] stops = new int[n];
+        for(int i = 0; i < n; i++) {
             stops[i] = scanner.nextInt();
         }
-
         System.out.println(computeMinRefills(dist, tank, stops));
     }
 }
